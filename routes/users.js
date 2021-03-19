34fb73b1212c366e7d73a03c8509c2dfcd1391b9
syoon <syoon@ijbinfo.com>
2021-03-19 13:33:20 +0900

    download

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/download', function(req, res, next) {
+  const file = './test.txt';
+  res.download(file);
+});
+
 module.exports = router;
